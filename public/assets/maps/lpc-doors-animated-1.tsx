18b1366115636ecc5e0bc48cdaa5c408c47bd591
Bozsóki Ádám <09.adam.b@gmail.com>
2020-04-04 16:59:41 +0200

    Add maps

---
+++ public/assets/maps/lpc-doors-animated-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="lpc-doors-animated-1" tilewidth="16" tileheight="16" tilecount="816" columns="24">
+ <image source="../lpc-doors-animated-1.png" width="384" height="544"/>
+</tileset>
