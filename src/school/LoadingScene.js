9f24e23704304f01066ede7a050ef2c2b9abfbcd
Bozsóki Ádám <09.adam.b@gmail.com>
2020-04-05 09:43:26 +0200

    add more sprites

--- src/school/LoadingScene.js
+++ src/school/LoadingScene.js
@@ -12,10 +12,10 @@ class LoadingScene extends Phaser.Scene {
     this.load.spritesheet("anna", "./assets/sprite/anna.png", {frameHeight: 64, frameWidth: 64});
     this.load.spritesheet("aaron", "./assets/sprite/aaron.png", {frameHeight: 64, frameWidth: 64});
     this.load.spritesheet("demon", "./assets/sprite/characters.png", {frameHeight: 64, frameWidth: 64});
-    this.load.spritesheet("mort", "./assets/sprite/mort.gif", {frameHeight: 64, frameWidth: 64});
-    this.load.spritesheet("tard", "./assets/sprite/tard.png", {frameHeight: 64, frameWidth: 64});
-    this.load.spritesheet("vita", "./assets/sprite/vita.png", {frameHeight: 64, frameWidth: 64});
-    this.load.spritesheet("doux", "./assets/sprite/doux.png", {frameHeight: 64, frameWidth: 64});
+    this.load.spritesheet("mort", "./assets/sprite/mort.gif", {frameHeight: 64, frameWidth : 64});
+    this.load.spritesheet("tard", "./assets/sprite/tard.gif", {frameHeight: 64, frameWidth: 64});
+    this.load.spritesheet("vita", "./assets/sprite/vita.gif", {frameHeight: 64, frameWidth: 64});
+    this.load.spritesheet("doux", "./assets/sprite/doux.gif", {frameHeight: 64, frameWidth: 64});
 
     this.load.tilemapTiledJSON(
       "digi_school_map",
